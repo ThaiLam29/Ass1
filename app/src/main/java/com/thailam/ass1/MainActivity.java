71384b4ca909c9467d6dc76c878adae7a3b78a9e
Thailam29 <lamntph34492@fpt.edu.vn>
2023-05-24 15:27:43 +0700

    Thailam

---
+++ app/src/main/java/com/thailam/ass1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.thailam.ass1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), dangnhap.class);
+                startActivity(intent);
+            }
+        },3000);
+    }
+}
